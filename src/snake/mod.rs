9f2ff090ad623e04ad85133ef845eff3f390e459
Marco Nuñez <marco.soulg@gmail.com>
2018-12-19 23:28:41 -0600

    Added movement

--- src/snake/mod.rs
+++ src/snake/mod.rs
@@ -4,8 +4,24 @@ pub struct Snake {
 }
 
 impl Snake {
-    fn new(location: (f32, f32)) -> Snake {
+    pub fn new(location: (f32, f32)) -> Snake {
         Snake { location, size: 1 }
+    }
+
+    pub fn move_on(&mut self, axis: &str) -> Result<(), &str> {
+        if axis == "x" {
+            self.location = (self.location.0 + 1.0, self.location.1);
+
+            return Ok(());
+        }
+
+        if axis == "y" {
+            self.location = (self.location.0, self.location.1 + 1.0);
+
+            return Ok(());
+        }
+
+        Err("No valid axis")
     }
 }
 
@@ -18,5 +34,30 @@ mod test {
         let s = Snake::new((0.0, 0.0));
 
         assert_eq!(s.size, 1);
+    }
+
+    #[test]
+    fn test_move_on_axis() {
+        let mut s = Snake::new((0.0, 0.0));
+
+        s.move_on("x").unwrap();
+
+        assert_eq!(s.location.0, 1.0);
+        assert_eq!(s.location.1, 0.0);
+
+        s.move_on("y").unwrap();
+
+        assert_eq!(s.location.0, 1.0);
+        assert_eq!(s.location.1, 1.0);
+    }
+
+    #[test]
+    fn test_get_error_on_wrong_axis() {
+        let mut s = Snake::new((0.0, 0.0));
+
+        if let Err(e) = s.move_on("w") {
+            assert_eq!(e, "No valid axis");
+        }
+
     }
 }
